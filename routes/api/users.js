e73a7267dc901d5327c042d447abd14dd792cafe
montygoldy <montygoldy2006@gmail.com>
2018-04-27 17:58:53 -0400

    Added passport and strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // @route  GET @api/users/test
 // @desc   Tests users route
@@ -79,5 +80,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route  GET @api/users/current
+// @desc   Return Current user route
+// @access PRIVATE
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
 
 //Body parser middleware
@@ -19,10 +20,13 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //Use Routes
-
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
