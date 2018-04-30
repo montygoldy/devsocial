9ed3357047a1a162e102129e0e08c85793459a18
montygoldy <montygoldy2006@gmail.com>
2018-04-30 16:12:42 -0400

    Added post request to post likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -93,4 +93,32 @@ router.delete(
   }
 );
 
+// @route  Post @api/posts/like/:id
+// @desc   Like posts
+// @access PRIVATE
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString === req.user.id)
+              .length > 0
+          )
+            return res
+              .status(400)
+              .json({ alreadyLiked: "User already liked this post" });
+
+          //Add user id to like array
+          post.likes.unshift({ user: req.user.id });
+          //Save to database
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postNotFound: "No post found" }));
+    });
+  }
+);
+
 module.exports = router;
