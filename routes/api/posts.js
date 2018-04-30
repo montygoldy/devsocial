148041faff5de508d9ef9e81bb6e59e98077c2a7
montygoldy <montygoldy2006@gmail.com>
2018-04-30 08:43:17 -0400

    Added route to get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,33 @@ const validatePostInput = require("../../validations/post");
 router.get("/test", (req, res) => res.json({ message: "posts works!" }));
 
 // @route  GET @api/posts
+// @desc   Read post
+// @access Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => res.status(404).json({ noPostsFound: "No posts found" }));
+});
+
+// @route  GET @api/posts/:id
+// @desc   Get post by id
+// @access Public
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err =>
+      res.status(404).json({ noPostFound: "No post found with that id" })
+    );
+});
+
+// @route  Post @api/posts
 // @desc   Create post
 // @access PRIVATE
 router.post(
