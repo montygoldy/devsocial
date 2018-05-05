98221da537fe8ec1f88214a2f2e93fd5aef8e3db
montygoldy <montygoldy2006@gmail.com>
2018-05-05 15:48:34 -0400

    Added basic form to register and login page as well as added onChange and onSubmit for the register form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,8 +2,36 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">
+              Sign in to your Code-network account
+            </p>
+            <form action="dashboard.html">
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address"
+                  name="email"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
