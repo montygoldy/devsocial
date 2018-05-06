f66a27b1e6bf52d963b90cc9e06f1a5d1e9306fa
montygoldy <montygoldy2006@gmail.com>
2018-05-06 14:13:47 -0400

    Finished login with validations

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
 //Register USer
 export const registerUser = (userData, history) => dispatch => {
@@ -7,4 +9,35 @@ export const registerUser = (userData, history) => dispatch => {
     .post("/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+//Login - Get User token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //Save to localstorage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      //Set token to auth header
+      setAuthToken(token);
+      //Decode token to get userData
+      const decoded = jwt_decode(token);
+      //Set Current USer
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in User
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
