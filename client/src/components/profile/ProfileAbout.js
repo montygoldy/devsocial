c15775791d1b79bcc21e8bf6487112ee0d36974e
montygoldy <montygoldy2006@gmail.com>
2018-05-10 19:27:28 -0400

    Added education and experience to the profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -20,7 +20,11 @@ class ProfileAbout extends Component {
           <div className="card card-body bg-light mb-3">
             <h3 className="text-center text-info">{firstName}'s Bio</h3>
             <p className="lead text-center">
-              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
+              {isEmpty(profile.bio) ? (
+                <span>()</span>
+              ) : (
+                <span>{firstName} doesn't have a bio</span>
+              )}
             </p>
             <hr />
             <h3 className="text-center text-info">Skill Set</h3>
