5bf0795c0c34749517a833b39e890549d0009dbe
montygoldy <montygoldy2006@gmail.com>
2018-05-11 18:28:02 -0400

    Finished comment feed and delete comment functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,11 +5,13 @@ import {
   GET_POSTS,
   POST_LOADING,
   DELETE_POST,
-  GET_POST
+  GET_POST,
+  CLEAR_ERRORS
 } from "./types";
 
 //ADD POST
 export const addPost = (postData) => dispatch => {
+  dispatch(clearErrors());
   axios.post("/api/posts", postData).then(res => dispatch({
     type: ADD_POST,
     payload: res.data
@@ -72,7 +74,19 @@ export const getPost = (id) => dispatch => {
 
 //ADD Comment
 export const addComment = (commentData, postId) => dispatch => {
+  dispatch(clearErrors());
   axios.post(`/api/posts/comment/${postId}`, commentData).then(res => dispatch({
+    type: GET_POST,
+    payload: res.data
+  })).catch(err => dispatch({
+    type: GET_ERRORS,
+    payload: err.response.data
+  }))
+}
+
+//Delete Comments
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios.delete(`/api/posts/comment/${postId}/${commentId}`).then(res => dispatch({
     type: GET_POST,
     payload: res.data
   })).catch(err => dispatch({
@@ -86,5 +100,12 @@ export const addComment = (commentData, postId) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  }
+}
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   }
 }
